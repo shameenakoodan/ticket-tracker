ed88b827b6c50cdc915c8bc0aa47960d1863feab
Shameena Koodan <shameena.koodan@gmail.com>
2023-07-23 10:19:49 -0500

    setup gh-pages

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -2,12 +2,19 @@ import Counter from "../Counter/Counter";
 import "./Employee.scss";
 const Employee =(props)=>{
     const {name,role} = props
+    let isDisabled = false;
+    const handleClick =(event)=>{
+        if (!isDisabled) {
+            isDisabled = true;
+          }
+    }
     return(
-        <div className="employee-details">
+        <div className="employee-details" >
             
             <p><label>Name  :   </label>{name}</p>
             <p><label>Role  :   </label>{role}</p>
             <Counter />
+            <button onClick={handleClick} >Submit</button>
         </div>
     );
 };
